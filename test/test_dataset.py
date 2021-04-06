de764ef0a6cfd6135bbb9372431a7c6b1b3b5588
Nestor Napoles Lopez <napulen@gmail.com>
2021-04-06 19:39:57 -0400

    Updating tests with new dictionary.

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -1,15 +1,15 @@
 import unittest
-from common import ANNOTATIONSCOREMAP, DATASPLITS
+from common import ANNOTATIONSCOREDUPLES, DATASPLITS
 
 class TestDataset(unittest.TestCase):
     def test_dataset_and_splits_length(self):
         train, val, test = DATASPLITS.values()
-        allAnnotations = list(ANNOTATIONSCOREMAP.keys())
+        allAnnotations = list(ANNOTATIONSCOREDUPLES.keys())
         allSplits = train + val + test
         self.assertEqual(len(allAnnotations), len(allSplits))
 
     def test_dataset_and_splits_equal(self):
         train, val, test = DATASPLITS.values()
-        allAnnotations = list(ANNOTATIONSCOREMAP.keys())
+        allAnnotations = list(ANNOTATIONSCOREDUPLES.keys())
         allSplits = train + val + test
         self.assertEqual(set(allAnnotations), set(allSplits))
