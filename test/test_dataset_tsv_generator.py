99767695de102b707b833680a627152abd74b069
Nestor Napoles Lopez <napulen@gmail.com>
2021-04-13 20:38:52 -0400

    Moving test_dataset to test_dataset_tsv_generator

--- test/test_dataset_tsv_generator.py
+++ test/test_dataset_tsv_generator.py
@@ -1797,6 +1797,20 @@ def _annotationScoreHashes():
 
 
 class TestDataset(unittest.TestCase):
+    def test_dataset_and_splits_length(self):
+        """ This one checks there are no duplicate entries. """
+        train, val, test = DATASPLITS.values()
+        allAnnotations = list(ANNOTATIONSCOREDUPLES.keys())
+        allSplits = train + val + test
+        self.assertEqual(len(allAnnotations), len(allSplits))
+
+    def test_dataset_and_splits_equal(self):
+        """ This one checks there are no missing/distinct entries."""
+        train, val, test = DATASPLITS.values()
+        allAnnotations = list(ANNOTATIONSCOREDUPLES.keys())
+        allSplits = train + val + test
+        self.assertEqual(set(allAnnotations), set(allSplits))
+
     def test_dataset_nicknames(self):
         """Checks that the identifiers ('nicknames') haven't changed. """
         self.maxDiff = 20000
