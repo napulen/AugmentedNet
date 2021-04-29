fa6232a33d62ff88ff2a9abf81df5c30f4082e3c
Nestor Napoles Lopez <napulen@gmail.com>
2021-04-28 22:22:59 -0400

    Adding filepath to ModelCheckpoint.

--- train.py
+++ train.py
@@ -157,7 +157,9 @@ def train(syntheticDataStrategy=None, modelName="simpleGRU"):
         callbacks=[
             EarlyStopping(monitor="val_localKey35_accuracy", patience=3),
             ModelCheckpoint(
-                monitor="val_localKey35_accuracy", save_best_only=True
+                "weights.{epoch:02d}-{val_loss:.2f}.hdf5",
+                monitor="val_localKey35_accuracy",
+                save_best_only=True,
             ),
         ],
     )
