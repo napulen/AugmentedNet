543a661b4389812acb23c1fc467271c6ed677c8f
Nestor Napoles Lopez <napulen@gmail.com>
2021-05-09 21:36:00 -0400

    More nodes in dense and gru

--- models.py
+++ models.py
@@ -18,8 +18,8 @@ def AugmentedNet(inputs, outputs, blocks=6):
         xi = layers.Input(shape=(sequenceLength, inputFeatures), name=name)
         x.append(xi)
         for i in range(blocks):
-            filters = 2**(blocks-1-i)
-            kernel = 2**i
+            filters = 2 ** (blocks - 1 - i)
+            kernel = 2 ** i
             h = layers.Conv1D(filters, kernel, padding="same")(xi)
             h = layers.BatchNormalization()(h)
             h = layers.Activation("relu")(h)
@@ -29,16 +29,16 @@ def AugmentedNet(inputs, outputs, blocks=6):
         inputs = layers.Concatenate()([xi for xi in xprime])
     else:
         inputs = xprime[0]
-    h = layers.Dense(64)(inputs)
+    h = layers.Dense(128)(inputs)
     h = layers.BatchNormalization()(h)
     h = layers.Activation("relu")(h)
-    h = layers.Dense(32)(h)
+    h = layers.Dense(64)(h)
     h = layers.BatchNormalization()(h)
     h = layers.Activation("relu")(h)
-    h = layers.Bidirectional(layers.GRU(30, return_sequences=True))(h)
+    h = layers.Bidirectional(layers.GRU(64, return_sequences=True))(h)
     h = layers.BatchNormalization()(h)
-    h = layers.Bidirectional(layers.GRU(30, return_sequences=True))(h)
-    h = layers.BatchNormalization()(h)
+    # h = layers.Bidirectional(layers.GRU(30, return_sequences=True))(h)
+    # h = layers.BatchNormalization()(h)
     y = []
     for output in outputs:
         outputFeatures = output.outputFeatures
