8471b2629690004156a701a08b93a54bd457d632
Nestor Napoles Lopez <napulen@gmail.com>
2021-05-09 21:45:24 -0400

    Increasing size of gru

--- models.py
+++ models.py
@@ -29,16 +29,16 @@ def AugmentedNet(inputs, outputs, blocks=6):
         inputs = layers.Concatenate()([xi for xi in xprime])
     else:
         inputs = xprime[0]
-    h = layers.Dense(128)(inputs)
+    h = layers.Dense(64)(inputs)
     h = layers.BatchNormalization()(h)
     h = layers.Activation("relu")(h)
-    h = layers.Dense(64)(h)
+    h = layers.Dense(32)(h)
     h = layers.BatchNormalization()(h)
     h = layers.Activation("relu")(h)
-    h = layers.Bidirectional(layers.GRU(64, return_sequences=True))(h)
+    h = layers.Bidirectional(layers.GRU(40, return_sequences=True))(h)
     h = layers.BatchNormalization()(h)
-    # h = layers.Bidirectional(layers.GRU(30, return_sequences=True))(h)
-    # h = layers.BatchNormalization()(h)
+    h = layers.Bidirectional(layers.GRU(40, return_sequences=True))(h)
+    h = layers.BatchNormalization()(h)
     y = []
     for output in outputs:
         outputFeatures = output.outputFeatures
