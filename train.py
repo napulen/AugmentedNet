5d7feab2d38ed90d405fcfdfea6e784ef397a519
Nestor Napoles Lopez <napulen@gmail.com>
2021-05-26 14:36:28 -0400

    RMSprop with learning_rate=0.01

--- train.py
+++ train.py
@@ -213,7 +213,7 @@ def train(
         initial_learning_rate=1e-2, decay_steps=600, decay_rate=0.9
     )
     model.compile(
-        optimizer=optimizers.RMSprop(),
+        optimizer=optimizers.RMSprop(learning_rate=0.01),
         loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
         metrics="accuracy",
     )
