abad48cccf433f369d3ab68733cf25d3aaba116b
Nestor Napoles Lopez <napulen@gmail.com>
2021-05-30 11:41:18 -0400

    Fixed learning rate. 0.003

--- train.py
+++ train.py
@@ -213,7 +213,7 @@ def train(
         boundaries=[8120], values=[0.01, 0.0001]
     )
     model.compile(
-        optimizer=optimizers.RMSprop(learning_rate=lr_schedule),
+        optimizer=optimizers.RMSprop(learning_rate=0.003),
         loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
         metrics="accuracy",
     )
