b1e6938d97b940320b8c0d97e24abb52d1e3873c
Nestor Napoles Lopez <napulen@gmail.com>
2021-07-06 12:26:27 -0400

    Removing commented code.

--- train.py
+++ train.py
@@ -390,15 +390,6 @@ if __name__ == "__main__":
         if arg == "experiment_name" or arg == "run_name":
             continue
         mlflow.log_param(f"custom_{arg}", value)
-    # mlflow.log_param("inputs", args.input_representations)
-    # mlflow.log_param("outputs", args.output_representations)
-    # mlflow.log_param("model", args.model)
-    # mlflow.log_param("syntheticDataStrategy", args.syntheticDataStrategy)
-    # mlflow.log_param("scrutinize_data", args.scrutinize_data)
-    # mlflow.log_param("sequenceLength", args.sequence_length)
-    # mlflow.log_param("testSetOn", args.test_set_on)
-    # mlflow.log_param("learningRateBoundaries", args.lr_boundaries)
-    # mlflow.log_param("learningRateValues", args.lr_values)
     timestamp = datetime.datetime.now().strftime("%y%m%dT%H%M%S")
     checkpoint = (
         f".model_checkpoint/"
