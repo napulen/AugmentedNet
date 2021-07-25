a4a8aff3e15e3c1c41b1bc6340d2211679a2c6e5
Nestor Napoles Lopez <napulen@gmail.com>
2021-07-24 21:47:24 -0400

    Adding tests for cli.

--- test/test_cli.py
+++ test/test_cli.py
@@ -2,8 +2,30 @@
 
 import unittest
 
-import AugmentedNet.cli
+from AugmentedNet import cli
 
 
-class TestEvaluate(unittest.TestCase):
-    pass
+class TestCli(unittest.TestCase):
+    def test_base_argument_names(self):
+        argsGT = set(cli.DefaultArguments._base.keys())
+        args = set(vars(cli._base().parse_args([])))
+        self.assertEqual(args, argsGT)
+
+    def test_tsv_argument_names(self):
+        argsGT = set(cli.DefaultArguments._tsv.keys())
+        args = set(vars(cli.tsv().parse_args([])))
+        parentArgs = set(vars(cli._base().parse_args([])))
+        self.assertEqual(args - parentArgs, argsGT)
+
+    def test_npz_argument_names(self):
+        argsGT = set(cli.DefaultArguments._npz.keys())
+        args = set(vars(cli.npz().parse_args([])))
+        parentArgs = set(vars(cli._base().parse_args([])))
+        self.assertEqual(args - parentArgs, argsGT)
+
+    def test_train_argument_names(self):
+        argsGT = set(cli.DefaultArguments._train.keys())
+        args = set(vars(cli.train().parse_args(["debug", "cli"])))
+        parentArgs = set(vars(cli.npz().parse_args([])))
+        positional = {"experiment_name", "run_name"}
+        self.assertEqual(args - parentArgs - positional, argsGT)
