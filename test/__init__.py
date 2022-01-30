dc2312e8659d31cbb882ccf3e04977cbc4bdf0d7
Néstor Nápoles López <napulen@gmail.com>
2022-01-29 20:16:02 -0500

    Changing test auxiliary_files structure for test_annotation_parser.

---
+++ test/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+class AuxiliaryFiles(object):
+    def __init__(self, modulepath):
+        testpath = os.path.dirname(os.path.realpath(__file__))
+        path = os.path.join(testpath, "auxiliary_files", modulepath)
+        files = os.listdir(path)
+        for file in files:
+            base, ext = file.rsplit(".")
+            filepath = os.path.join(path, file)
+            setattr(self, base, filepath)
