b5d2129f9102bdeea390c74bb878139158160876
Néstor Nápoles López <napulen@gmail.com>
2022-02-21 16:33:51 -0500

    First attempt at texturization-on-transposition. Didn't work.

--- AugmentedNet/dataset_npz_generator.py
+++ AugmentedNet/dataset_npz_generator.py
@@ -5,16 +5,17 @@ import pandas as pd
 import numpy as np
 
 from . import cli
+from . import joint_parser
 from .cache import TransposeKey
-from .common import DATASETSUMMARYFILE
+from .common import DATASETSUMMARYFILE, ANNOTATIONSCOREDUPLES
 from .feature_representation import KEYS, INTERVALCLASSES
 from .input_representations import (
     available_representations as availableInputs,
 )
-from .joint_parser import J_LISTTYPE_COLUMNS
 from .output_representations import (
     available_representations as availableOutputs,
 )
+from .score_parser import _recursiveTexturization
 from .utils import padToSequenceLength
 
 
@@ -23,14 +24,45 @@ def _getTranspositions(df):
     tonicizedKeys = set(tonicizedKeys)
     ret = []
     for interval in INTERVALCLASSES:
-        if interval == "P1":
-            continue
         transposed = [TransposeKey(k, interval) for k in tonicizedKeys]
         # Transpose to this interval if every modulation lies within
         # the set of KEY classes that we can classify
         if set(transposed).issubset(set(KEYS)):
             ret.append(interval)
     return ret
+
+
+def initializeArrays(inputRepresentations, outputRepresentations):
+    """Each array becomes a dict entry with the name of the input/output"""
+    outputArrays = {}
+    for split in ["training", "validation"]:
+        for x in inputRepresentations:
+            outputArrays[split + f"_X_{x}"] = []
+        for y in outputRepresentations:
+            outputArrays[split + f"_y_{y}"] = []
+    return outputArrays
+
+
+def scrutinize(df, qualityThresh=0.75, bassThresh=0.8):
+    """Filter 'bad quality' annotations."""
+    originalIndex = len(df.index)
+    df = df[
+        (df.qualitySquaredSum < qualityThresh)
+        & (df.measureMisalignment == False)
+        & (df.incongruentBass < bassThresh)
+    ]
+    filteredIndex = len(df.index)
+    print(f"\t({originalIndex}, {filteredIndex})")
+
+
+def correctSplit(split, testSetOn):
+    """Correct the split of this file according to 'testSetOn' parameter."""
+    if testSetOn:
+        if split == "validation":
+            return "training"
+        elif split == "test":
+            return "validation"
+    return split
 
 
 def generateDataset(
@@ -46,12 +78,9 @@ def generateDataset(
     tsvDir,
     npzOutput,
 ):
-    outputArrays = {}
-    for split in ["training", "validation"]:
-        for x in inputRepresentations:
-            outputArrays[split + f"_X_{x}"] = []
-        for y in outputRepresentations:
-            outputArrays[split + f"_y_{y}"] = []
+    outputArrays = initializeArrays(
+        inputRepresentations, outputRepresentations
+    )
     training = ["training", "validation"] if testSetOn else ["training"]
     validation = ["test"] if testSetOn else ["validation"]
     datasetDir = f"{tsvDir}-synth" if synthetic else tsvDir
@@ -70,57 +99,38 @@ def generateDataset(
     ]
     df = pd.concat([trainingdf, validationdf])
     for row in df.itertuples():
-        split = row.split
-        if testSetOn:
-            if split == "validation":
-                split = "training"
-            elif split == "test":
-                split = "validation"
-        else:
-            if split == "test":
-                continue
+        split = correctSplit(row.split, testSetOn)
+        if split == "test":
+            # Preemptive measure just to avoid a potential disaster
+            continue
         print(f"{row.split} -used-as-> {split}", row.file)
-        tsvlocation = os.path.join(datasetDir, row.split, row.file)
-        df = pd.read_csv(tsvlocation + ".tsv", sep="\t")
-        for col in J_LISTTYPE_COLUMNS:
-            df[col] = df[col].apply(eval)
-        # Filter the bad quality annotations
+        tsvlocation = os.path.join(datasetDir, row.split, f"{row.file}.tsv")
+        df = joint_parser.from_tsv(tsvlocation)
         if scrutinizeData and split == "training":
-            originalIndex = len(df.index)
-            df = df[
-                (df.qualitySquaredSum < 0.75)
-                & (df.measureMisalignment == False)
-                & (df.incongruentBass < 0.8)
-            ]
-            filteredIndex = len(df.index)
-            print(f"\t({originalIndex}, {filteredIndex})")
+            df = scrutinize(df)
         if dataAugmentation and split == "training":
             transpositions = _getTranspositions(df)
             print("\t", transpositions)
         else:
             transpositions = ["P1"]
-        for inputRepresentation in inputRepresentations:
-            inputLayer = availableInputs[inputRepresentation](df)
-            Xi = inputLayer.array
-            Xi = padToSequenceLength(Xi, sequenceLength)
-            for transposition in transpositions:
-                tr = inputLayer.run(transposition=transposition)
-                tr = padToSequenceLength(tr, sequenceLength)
-                Xi = np.concatenate((Xi, tr))
-            npzfile = f"{split}_X_{inputRepresentation}"
-            for sequence in Xi:
-                outputArrays[npzfile].append(sequence)
-        for outputRepresentation in outputRepresentations:
-            outputLayer = availableOutputs[outputRepresentation](df)
-            yi = outputLayer.array
-            yi = padToSequenceLength(yi, sequenceLength)
-            for transposition in transpositions:
-                tr = outputLayer.run(transposition=transposition)
-                tr = padToSequenceLength(tr, sequenceLength)
-                yi = np.concatenate((yi, tr))
-            npzfile = f"{split}_y_{outputRepresentation}"
-            for sequence in yi:
-                outputArrays[npzfile].append(sequence)
+        # if synthetic:
+        #     dfsynth = df[df.a_harmonicRhythm == 0]
+        for transposition in transpositions:
+            if synthetic:
+                annotation = ANNOTATIONSCOREDUPLES[row.file][0]
+                df = joint_parser.parseAnnotationAndAnnotation(annotation)
+            for inputRepresentation in inputRepresentations:
+                inputLayer = availableInputs[inputRepresentation](df)
+                Xi = inputLayer.run(transposition=transposition)
+                Xi = padToSequenceLength(Xi, sequenceLength)
+                npzfile = f"{split}_X_{inputRepresentation}"
+                outputArrays[npzfile].append(Xi)
+            for outputRepresentation in outputRepresentations:
+                outputLayer = availableOutputs[outputRepresentation](df)
+                yi = outputLayer.run(transposition=transposition)
+                yi = padToSequenceLength(yi, sequenceLength)
+                npzfile = f"{split}_y_{outputRepresentation}"
+                outputArrays[npzfile].append(yi)
     # drop the extension, we'll overwrite it to .npz
     filename, _ = os.path.splitext(npzOutput)
     outputFile = f"{filename}-synth" if synthetic else filename
