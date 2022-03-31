492221f42fde0efd27a31b9ecfb5a35b2e27e8cf
Néstor Nápoles López <napulen@gmail.com>
2022-03-31 09:50:20 -0400

    Wrote a script to sonify the input encoding of the network.

---
+++ misc/sonifyencoding.py
@@ -0,0 +1,21 @@
+import music21
+import argparse
+from AugmentedNet.score_parser import parseScore
+
+if __name__ == "__main__":
+    f = "/mnt/c/Users/nesto/OneDrive/Desktop/langscoreonly-encoding.mxl"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_musicxml", help="An input file to encode/sonify.")
+    args = parser.parse_args([f])
+    df = parseScore(args.input_musicxml)
+    bassPart = music21.stream.Part()
+    chromaPart = music21.stream.Part()
+    for n in df.s_notes:
+        bass = f"{n[0][:-1]}3"
+        chroma = list(dict.fromkeys([f"{x[:-1]}4" for x in n]))
+        bassPart.append(music21.note.Note(bass, quarterLength=0.125))
+        chromaPart.append(music21.chord.Chord(chroma, quarterLength=0.125))
+        print(bass, chroma)
+    s = music21.stream.Stream([chromaPart, bassPart])
+    s.show("text")
+    s.show()
