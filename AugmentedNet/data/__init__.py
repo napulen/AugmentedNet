248a2b2f2b932495cbe573a9232e84cddb1a6c7a
Néstor Nápoles López <napulen@gmail.com>
2022-06-16 09:15:05 -0400

    Adding mps to the available_datasets.

--- AugmentedNet/data/__init__.py
+++ AugmentedNet/data/__init__.py
@@ -1,9 +1,10 @@
-from . import abc_dcml, bps, haydnsun, tavern, wir, wirwtc
+from . import abc_dcml, bps, haydnsun, mps, tavern, wir, wirwtc
 
 available_collections = {
     "abc": abc_dcml,
     "bps": bps,
     "haydnop20": haydnsun,
+    "mps": mps,
     "tavern": tavern,
     "wir": wir,
     "wirwtc": wirwtc,
