0094e8aafa3c879e5f1c1b2c81355ccd947a0496
Néstor Nápoles López <napulen@gmail.com>
2022-06-21 16:33:34 -0400

    Modifying sonifyencoding.

--- misc/sonifyencoding.py
+++ misc/sonifyencoding.py
@@ -2,8 +2,18 @@ import music21
 import argparse
 from AugmentedNet.score_parser import parseScore
 
+noteoctaves = {
+    "C": 4,
+    "E": 4,
+    "G": 4,
+    "B": 4,
+    "D": 5,
+    "F": 5,
+    "A": 5,
+}
+
 if __name__ == "__main__":
-    f = "/mnt/c/Users/nesto/OneDrive/Desktop/langscoreonly-encoding.mxl"
+    f = "/mnt/d/thesis_figures/claraschumann.mxl"
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "input_musicxml", help="An input file to encode/sonify."
@@ -14,7 +24,7 @@ if __name__ == "__main__":
     chromaPart = music21.stream.Part()
     for n in df.s_notes:
         bass = f"{n[0][:-1]}3"
-        chroma = list(dict.fromkeys([f"{x[:-1]}4" for x in n]))
+        chroma = list(dict.fromkeys([f"{x[:-1]}{noteoctaves[x[0]]}" for x in n]))
         bassPart.append(music21.note.Note(bass, quarterLength=0.125))
         chromaPart.append(music21.chord.Chord(chroma, quarterLength=0.125))
         print(bass, chroma)
