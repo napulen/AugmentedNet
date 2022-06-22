cc702a761ce3fb1e819f9d49eaf14ca458af38a7
Néstor Nápoles López <napulen@gmail.com>
2022-06-21 23:13:23 -0400

    Updating texturize_example.

--- misc/texturize_example.py
+++ misc/texturize_example.py
@@ -1,30 +1,67 @@
-from common import ANNOTATIONSCOREDUPLES
-from score_parser import (
+import random
+
+import music21
+
+from AugmentedNet.common import ANNOTATIONSCOREDUPLES
+from AugmentedNet.score_parser import (
     _m21Parse,
     _initialDataFrame,
     _texturizeAnnotationScore,
 )
-from score_parser import parseAnnotationAsScore
-import music21
+from AugmentedNet.score_parser import parseAnnotationAsScore
 
 
-def _engraveScore(df):
+interesting = [
+    "abc-op18-no1-1",
+    "bps-22-op054-1",
+    "bps-25-op079-sonatina-1",
+    "haydnop20-no2-3",
+    "haydnop20-no4-1",
+    "tavern-beethoven-woo-69-b",
+    "tavern-beethoven-woo-70-a",
+    "tavern-beethoven-woo-70-b",
+    "tavern-beethoven-woo-71-a",
+    "wir-bach-chorales-12",
+    "wir-bach-chorales-17",
+    "wir-openscore-liedercorpus-chaminade-amoroso",
+    "wir-openscore-liedercorpus-reichardt-zwolf-gesange-op-3-03-die-blume-der-blumen",
+    "wir-openscore-liedercorpus-schubert-winterreise-d-911-02-die-wetterfahne",
+    "wir-openscore-liedercorpus-schubert-winterreise-d-911-05-der-lindenbau",
+]
+
+
+def _engraveScore(df, ts):
     """Useful for debugging _texturizeAnnotationScore."""
     chords = music21.stream.Stream()
+    offset = 0.0
     for row in df.itertuples():
+        if offset in ts:
+            chords.append(music21.meter.TimeSignature(ts[offset]))
         if row.s_measure == 0:
+            offset += row.s_duration
             continue
         pitches = row.s_notes
         duration = row.s_duration
         chord = music21.chord.Chord(pitches, quarterLength=duration)
         chords.append(chord)
+        offset += duration
     return chords
 
 
-f = "bps-01-op002-no1-1"
+def get_ts(score):
+    s = music21.converter.parse(score)
+    tss = {
+        ts.offset: ts.ratioString
+        for ts in s.flat.getElementsByClass("TimeSignature")
+    }
+    return tss
 
-annotation, score = ANNOTATIONSCOREDUPLES[f]
 
-df = parseAnnotationAsScore(annotation, texturize=True, eventBased=True)
-
-_engraveScore(df).show()
+if __name__ == "__main__":
+    random.shuffle(interesting)
+    nickname = interesting[0]
+    print(nickname)
+    annotation, score = ANNOTATIONSCOREDUPLES[nickname]
+    tss = get_ts(score)
+    df = parseAnnotationAsScore(annotation, texturize=True, eventBased=True)
+    _engraveScore(df, tss).show()
